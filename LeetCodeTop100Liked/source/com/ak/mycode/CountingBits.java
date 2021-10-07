7b721780cda5af20d90d7fe18cd575678e7d69ec
arun.ms@zohocorp <arun.ms@zohocorp.com>
2021-10-07 19:01:28 +0530

    CountingBits
    https://leetcode.com/problems/counting-bits/

---
+++ LeetCodeTop100Liked/source/com/ak/mycode/CountingBits.java
@@ -0,0 +1,26 @@
+package com.ak.mycode;
+
+//https://leetcode.com/problems/counting-bits/
+public class CountingBits {
+
+    public static void main(String[] args) {
+        int[] result = new CountingBits().countBits(5);
+        for(int i : result) System.out.println(i);
+    }
+
+
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        result[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & 1) == 0) { //even
+                // to count the number of 1s
+                result[i] = result[i >>> 1];
+            } else {
+                // odd number's 1s count always +1 than previous even number
+                result[i] = result[i - 1] + 1;
+            }
+        }
+        return result;
+    }
+}
