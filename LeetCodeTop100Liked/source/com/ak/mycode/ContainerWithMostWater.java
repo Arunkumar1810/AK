b96a5d7655106f214ab08db75fa77222ba7b3ebe
arun.ms@zohocorp <arun.ms@zohocorp.com>
2021-10-08 14:46:47 +0530

    ContainerWithMostWater
    https://leetcode.com/problems/container-with-most-water/

---
+++ LeetCodeTop100Liked/source/com/ak/mycode/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package com.ak.mycode;
+
+//https://leetcode.com/problems/container-with-most-water/
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        System.out.println("Result : "+new ContainerWithMostWater().maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+
+    public int maxArea(int[] height) {
+        int result = 0;
+        int temp = 0;
+        for (int i=0,j=height.length-1;i<j;) {
+            temp = Math.min(height[i],height[j]) * (j-i);
+            result = Math.max(temp,result);
+            if(height[i]<height[j]) i++;
+            else j--;
+        }
+        return result;
+    }
+}
