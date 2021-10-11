b368b36126b9ccc22800582a2df58c59617ce291
arun.ms@zohocorp <arun.ms@zohocorp.com>
2021-10-12 00:22:59 +0530

    InvertBinaryTree
    https://leetcode.com/problems/invert-binary-tree/

--- LeetCodeTop100Liked/source/com/ak/mycode/InvertBinaryTree.java
+++ LeetCodeTop100Liked/source/com/ak/mycode/InvertBinaryTree.java
@@ -13,15 +13,13 @@ public class InvertBinaryTree {
     }
 
     public TreeNode invertTree(TreeNode root) {
-        if (root != null) {
-            TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
-            if (root.left != null) invertTree(root.left);
-            if (root.right != null) invertTree(root.right);
-            return root;
-        }
-        return null;
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        if (root.left != null) invertTree(root.left);
+        if (root.right != null) invertTree(root.right);
+        return root;
     }
 
     public TreeNode addElement(Integer[] arr, int index) {
