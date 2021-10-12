643100ececefbcafb89b1c55b03e701949da7299
arun.ms@zohocorp <arun.ms@zohocorp.com>
2021-10-12 22:54:09 +0530

    BinarySearch
    https://leetcode.com/problems/binary-search

--- studyplan/algorithms/binarysearch/source/com/ak/mycode/BinarySearch.java
+++ studyplan/algorithms/binarysearch/source/com/ak/mycode/BinarySearch.java
@@ -4,20 +4,19 @@ package com.ak.mycode;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        System.out.println("Result : "+new BinarySearch().search(new int[]{5},-5));
+        System.out.println("Result : "+new BinarySearch().search(new int[]{1,2,3,4,5},5));
     }
 
     public int search(int[] nums, int target) {
         int leftIndex = 0;
         int rightIndex = nums.length-1;
-        while (rightIndex-leftIndex > 1) {
-            int index = (leftIndex + rightIndex)/2;
-            if(nums[index]<target) leftIndex = index;
+        while (leftIndex<rightIndex) {
+            int index = leftIndex + (rightIndex - leftIndex)/2;
+            if(nums[index]<target) leftIndex = index + 1;
             else if (nums[index]>target) rightIndex = index;
             else return index;
         }
         if(nums[leftIndex] == target) return leftIndex;
-        if(nums[rightIndex] == target) return rightIndex;
         return -1;
     }
 }
