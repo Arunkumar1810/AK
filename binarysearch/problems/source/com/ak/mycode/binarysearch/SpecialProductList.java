7ca0a320166ecbebafcbedeed41b6e80fa7e2caf
arun.ms@zohocorp <arun.ms@zohocorp.com>
2021-10-27 21:46:32 +0530

    SpecialProductList
    //https://binarysearch.com/problems/Special-Product-List
    
    //Array
    
    //two pointers

---
+++ binarysearch/problems/source/com/ak/mycode/binarysearch/SpecialProductList.java
@@ -0,0 +1,35 @@
+package com.ak.mycode.binarysearch;
+
+import java.util.Arrays;
+
+//https://binarysearch.com/problems/Special-Product-List
+
+//Array
+
+//two pointers
+
+public class SpecialProductList {
+
+    public static void main(String[] args) {
+        int[] result = new SpecialProductList().solve(new int[]{1, 2, 3, 4, 5});
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] solve(int[] nums) {
+        int[] result = new int[nums.length];
+        int frontVal = 1;
+        int backVal = 1;
+        Arrays.fill(result,1);
+        for (int i = 0,j=nums.length-1 ; i<nums.length && j>=0;i++,j--) {
+            result[i] = result[i] * frontVal;
+            frontVal = nums[i] * frontVal;
+
+            result[j] = result[j] * backVal ;
+            backVal = nums[j] * backVal;
+
+        }
+        return result;
+    }
+}
