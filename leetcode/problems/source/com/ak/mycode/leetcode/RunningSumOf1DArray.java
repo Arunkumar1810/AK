c72c1ea50129542e67cb986e588c815c1c8b5160
AK <arunkumar1.ak700@gmail.com>
2022-01-18 20:41:38 +0530

    practice

--- leetcode/problems/source/com/ak/mycode/leetcode/RunningSumOf1DArray.java
+++ leetcode/problems/source/com/ak/mycode/leetcode/RunningSumOf1DArray.java
@@ -9,8 +9,8 @@ public class RunningSumOf1DArray {
     }
 
     public int[] runningSum(int[] nums) {
-        for(int i=1;i<nums.length;i++) {
-            nums[i]+=nums[i-1];
+        for (int i=1;i<nums.length;i++) {
+            nums[i] += nums[i-1];
         }
         return nums;
     }
