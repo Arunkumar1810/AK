2724c418ed98a5f5e0066a44eed80d53b5fd7449
AK <arunkumar1.ak700@gmail.com>
2022-04-18 19:51:10 +0530

    GenerateParentheses

--- leetcode/problems/source/com/ak/mycode/leetcode/GenerateParentheses.java
+++ leetcode/problems/source/com/ak/mycode/leetcode/GenerateParentheses.java
@@ -1,6 +1,7 @@
 package com.ak.mycode.leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class GenerateParentheses {
@@ -11,10 +12,34 @@ public class GenerateParentheses {
     }
 
     public List<String> generateParenthesis(int n) {
+        List<String>[] dp = new ArrayList[n+1];
         List<String> result = new ArrayList<>();
-        for (int i=1;i<n;i++) {
-            result = generateParenthesis(i);
+        result.add("");
+        dp[0] = result;
+        for(int i = 1;i<=n;i++) {
+            dp[i] = generateParenthesis(i,dp);
         }
+        return dp[n];
+    }
+
+    public List<String> generateParenthesis(int n, List<String>[] dp) {
+        List<String> result = new ArrayList<>();
+        for(String pattern : dp[n-1]) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("(");
+            sb.append(pattern);
+            sb.append(")");
+            result.add(sb.toString());
+        }
+        HashSet<String> res = new HashSet<>();
+        for(int i=1;i<n;i++) {
+            for(String part1 : dp[i]) {
+                for(String part2 : dp[n-i]) {
+                    res.add(new StringBuilder().append(part1).append(part2).toString());
+                }
+            }
+        }
+        result.addAll(res);
         return result;
     }
 }
